a3a971d6b4a90b2a8727701ca8fb659a146f7b0d
Jackie Chan <jackiechanmakes@gmail.com>
2025-04-02 13:30:43 -0700

    Add React and clean up data architecture

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// API endpoint to fetch data
+app.get('/data', (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  // Pass startDate and endDate to the C program
+  const command = `./sensor_data_program ${startDate} ${endDate}`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send('Error executing C program');
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(500).send('Error in C program');
+    }
+
+    // Parse the output from C program and send it as JSON
+    const data = JSON.parse(stdout);
+    res.json(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ data/fetch_data.c
@@ -0,0 +1,272 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <wiringPiI2C.h>
+#include <string.h>
+#include <time.h>
+
+#include <mysql/mysql.h>
+#include <stdio.h>
+
+#define MAXTIMINGS 85
+#define DHTPIN 7
+
+void insert_db();
+void fetch_data();
+
+// LCD information
+int LCDAddr = 0x27;
+int BLEN = 1;
+int fd;
+
+void write_word(int data){
+	int temp = data;
+	if ( BLEN == 1 )
+		temp |= 0x08;
+	else
+		temp &= 0xF7;
+	wiringPiI2CWrite(fd, temp);
+}
+
+void send_command(int comm){
+	int buf;
+	// Send bit7-4 firstly
+	buf = comm & 0xF0;
+	buf |= 0x04;			// RS = 0, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+
+	// Send bit3-0 secondly
+	buf = (comm & 0x0F) << 4;
+	buf |= 0x04;			// RS = 0, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+}
+
+void send_data(int data){
+	int buf;
+	// Send bit7-4 firstly
+	buf = data & 0xF0;
+	buf |= 0x05;			// RS = 1, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+
+	// Send bit3-0 secondly
+	buf = (data & 0x0F) << 4;
+	buf |= 0x05;			// RS = 1, RW = 0, EN = 1
+	write_word(buf);
+	delay(2);
+	buf &= 0xFB;			// Make EN = 0
+	write_word(buf);
+}
+
+void init(){
+	send_command(0x33);	// Must initialize to 8-line mode at first
+	delay(5);
+	send_command(0x32);	// Then initialize to 4-line mode
+	delay(5);
+	send_command(0x28);	// 2 Lines & 5*7 dots
+	delay(5);
+	send_command(0x0C);	// Enable display without cursor
+	delay(5);
+	send_command(0x01);	// Clear Screen
+	wiringPiI2CWrite(fd, 0x08);
+}
+
+void clear(){
+	send_command(0x01);	//clear Screen
+}
+
+void write(int x, int y, char data[]){
+	int addr, i;
+	int tmp;
+	if (x < 0)  x = 0;
+	if (x > 15) x = 15;
+	if (y < 0)  y = 0;
+	if (y > 1)  y = 1;
+
+	// Move cursor
+	addr = 0x80 + 0x40 * y + x;
+	send_command(addr);
+
+	tmp = strlen(data);
+	for (i = 0; i < tmp; i++){
+		send_data(data[i]);
+	}
+}
+
+void writeLCD(char* data){
+	fd = wiringPiI2CSetup(LCDAddr);
+	init();
+    write(0, 0, " humi temp time");
+    write(1, 1, data);
+	// clear();
+}
+
+// Humdifier 
+int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+
+void read_dht11_dat()
+{
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
+    float f;
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+    pinMode( DHTPIN, OUTPUT );
+    digitalWrite( DHTPIN, LOW );
+    delay( 18 );
+    digitalWrite( DHTPIN, HIGH );
+    delayMicroseconds( 40 );
+    pinMode( DHTPIN, INPUT );
+    for ( i = 0; i < MAXTIMINGS; i++ )
+    {
+        counter = 0;
+        while ( digitalRead( DHTPIN ) == laststate )
+        {
+            counter++;
+            delayMicroseconds( 1 );
+            if ( counter == 255 )
+            {
+                break;
+            }
+        }
+        laststate = digitalRead( DHTPIN );
+        if ( counter == 255 )
+            break;
+        if ( (i >= 4) && (i % 2 == 0) )
+        {
+            dht11_dat[j / 8] <<= 1;
+            if ( counter > 16 )
+                dht11_dat[j / 8] |= 1;
+            j++;
+        }
+    }
+    if ( (j >= 40) &&
+            (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+    {   
+        // Format data for LCD
+        char lcd_data[16] = "";
+        char humidifier_data[5];
+        char temperature_data[5];
+        char time_data[6];
+
+        // Humidifier and Temp data
+        sprintf(humidifier_data, "%d.%d",  dht11_dat[0], dht11_dat[1]);
+        sprintf(temperature_data, "%d.%d",  dht11_dat[2], dht11_dat[3]);
+        strcat(lcd_data, humidifier_data);
+        strcat(lcd_data, " ");
+        strcat(lcd_data, temperature_data);
+        strcat(lcd_data, " ");
+
+        // Time data
+        time_t current_time;
+        struct tm * time_info;
+        time(&current_time);
+        time_info = localtime(&current_time);
+        strftime(time_data, 6, "%H:%M", time_info);
+        strcat(lcd_data, time_data);
+
+        writeLCD(lcd_data);
+
+        insert_db();
+        // Print to console
+        // printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F) Time = %s\n",
+        //         dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f, time_data);
+        fetch_data();
+    }
+    else
+    {
+        printf( "Data not good, skip\n" );
+    }
+}
+
+MYSQL *conn;
+MYSQL_RES *res;
+MYSQL_ROW row;
+
+void connect_db() {
+    char *server = "localhost";
+    char *user = "john";
+    char *password = "password";
+    char *database = "studentdb";
+
+    conn = mysql_init(NULL);
+    /* Connect to database */
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+}
+
+void disconnect_db() {
+    /* Close connection */
+    mysql_free_result(res);
+    mysql_close(conn);
+}
+
+void insert_db() {
+    char sqlInsertStatement[100];
+    sprintf(sqlInsertStatement, "INSERT INTO sensor_data (humidity, temperature, timestamp) VALUES (%d.%d, %d.%d, NOW())", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
+
+    /* Send SQL insert query */
+    if ( mysql_query(conn, sqlInsertStatement))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_use_result(conn);
+}
+
+void fetch_data() {
+    /* Send SQL select query */
+    if (mysql_query(conn, "select temperature, humidity, timestamp from sensor_data ORDER BY timestamp DESC LIMIT 1"))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_store_result(conn);
+
+    row = mysql_fetch_row(res);
+    
+    if (res) {
+        printf("Temperature: %s\n", row[0]);
+        printf("Humidity: %s\n", row[1]);
+        printf("Timestamp: %s\n", row[2]);
+    }
+
+    /* SQL select query output */
+    // printf("\n\nData in sensor_data\n");
+    // printf("\nID\tHUMIDITY\tTEMP\tTIMESTAMP\n");
+    // while ((row = mysql_fetch_row(res)) != NULL)
+    //     printf("%s\t%s\t%s\t%s\n", row[0], row[1], row[2], row[3]);
+
+}
+
+void test() {
+    printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+}
+
+int main( void )
+{
+    // printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+    if ( wiringPiSetup() == -1 )
+        exit( 1 );
+
+    connect_db();
+
+    read_dht11_dat();
+    
+    disconnect_db();
+    return(0);
+}
+
+
